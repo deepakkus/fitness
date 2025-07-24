1c6be912b434fcdb1111ecc5cd0684a1cdcdc62a
deepakkus <deepak09.singh@gmail.com>
2025-07-24 19:16:03 +0530

    code updated

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -16,9 +16,10 @@ import { PrismaClient } from "@prisma/client";
 // https://pris.ly/d/help/next-js-best-practices
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
+if (process.env.DISABLE_DB !== "true"){
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
