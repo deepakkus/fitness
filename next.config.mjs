b4b5d2cea96b9be8c463311db71a8ec613329906
deepakkus <deepak09.singh@gmail.com>
2025-07-25 18:33:56 +0530

    code updated

--- next.config.mjs
+++ next.config.mjs
@@ -33,11 +33,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_BASE_URL,
+        hostname: fitness-theta-beryl.vercel.app,
         pathname: '/api/images/**',
       },
     ],
-	unoptimized: true
   },
 };
 
