8e5693b38d827ee0daa57ec877fdc508c8c7ad7b
deepakkus <deepak09.singh@gmail.com>
2025-07-25 17:14:26 +0530

    code updated

--- next.config.mjs
+++ next.config.mjs
@@ -33,7 +33,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_IMAGE_HOST,
+        hostname: '**.vercel.app',
         pathname: '/api/images/**',
       },
     ],
